fdcbbbf938b0c0a5f20d0dcae72ed3fd8d72e9f4
MD Sadid <sfhsadid@gmail.com>
2024-11-07 09:43:12 +0300

    problem 4 done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 {
   //
   //Problem 1
-  type numberArrar = number[];
-  function sumArray(param: numberArrar): number {
+  type globalArray<T> = Array<T>;
+  function sumArray(param: globalArray<number>): number {
     let getSumNumber: number = 0;
     for (let index = 0; index < param.length; index++) {
       getSumNumber += param[index];
@@ -12,9 +12,9 @@
   // console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
   // Problem 2
-  function removeDuplicates(param: numberArrar): numberArrar {
-    let sortArray: numberArrar = param.slice().sort((a, b) => a - b);
-    let getSingle: numberArrar = [];
+  function removeDuplicates(param: globalArray<number>): globalArray<number> {
+    let sortArray: globalArray<number> = param.slice().sort((a, b) => a - b);
+    let getSingle: globalArray<number> = [];
 
     for (let index = 0; index < sortArray.length; index++) {
       if (!(sortArray[index] === sortArray[index - 1])) {
@@ -27,7 +27,7 @@
 
   // problem 3
   function countWordOccurrences(param1: string, param2: string): number {
-    const stringArray: string[] = param1
+    const stringArray: globalArray<string> = param1
       .toLowerCase()
       .replace("!", "")
       .split(" ");
@@ -39,12 +39,40 @@
     }
     return count;
   }
-  console.log(
-    countWordOccurrences(
-      "TypeScript is great. I love TypeScript!",
-      "typescript"
-    )
-  );
+  // console.log(
+  //   countWordOccurrences(
+  //     "TypeScript is great. I love TypeScript!",
+  //     "typescript"
+  //   )
+  // );
+
+  // Problem 4
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
+  type RectangleArea = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
+  type ShapeArea = Circle | RectangleArea;
+  function calculateShapeArea(shape: ShapeArea): number {
+    if (shape.shape === "circle") {
+      return parseFloat((Math.PI * Math.pow(shape.radius, 2)).toFixed(2));
+    } else if (shape.shape === "rectangle") {
+      return shape.width * shape.height;
+    }
+    return 0;
+  }
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+  const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  });
+  // console.log(circleArea);
+  // console.log(rectangleArea);
 
   //
 }
