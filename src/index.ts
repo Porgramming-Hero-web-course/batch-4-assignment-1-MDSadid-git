210000655cc5ac4ee10a6ccf580ec56348816772
MD Sadid <sfhsadid@gmail.com>
2024-11-07 10:32:30 +0300

    Problem 5 done

--- src/index.ts
+++ src/index.ts
@@ -74,5 +74,21 @@
   // console.log(circleArea);
   // console.log(rectangleArea);
 
+  // problem 5
+  type Person = {
+    name: string;
+    age: number;
+  };
+  function getProperty2(param1: Person, param2: keyof Person): string | number {
+    const OwenVelue = param2;
+    return param1[OwenVelue];
+  }
+  function getProperty<T, K extends keyof T>(myObj: T, key: K): T[K] {
+    return myObj[key];
+  }
+  const person: Person = { name: "Alice", age: 30 };
+  console.log(getProperty(person, "name"));
+  console.log(getProperty2(person, "name"));
+
   //
 }
