d5f11cad9d065b18c3186c4c5a8e68a2a3ee6602
MD Sadid <sfhsadid@gmail.com>
2024-11-07 07:49:52 +0300

    problem 1 done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hi Sadid");
+{
+  //
+  //Problem 1
+  type numberArrar = number[];
+  function sumArray(param: numberArrar): number {
+    let getSumNumber: number = 0;
+    for (let index = 0; index < param.length; index++) {
+      getSumNumber += param[index];
+    }
+    return getSumNumber;
+  }
+  console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+  //
+}
