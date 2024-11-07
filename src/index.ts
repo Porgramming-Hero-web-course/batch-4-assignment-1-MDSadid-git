8e76af136f8ae8e8495371fae4a067391806ebe1
MD Sadid <sfhsadid@gmail.com>
2024-11-07 08:24:37 +0300

    problem 2 done

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,21 @@
     }
     return getSumNumber;
   }
-  console.log(sumArray([1, 2, 3, 4, 5, 6]));
+  // console.log(sumArray([1, 2, 3, 4, 5, 6]));
+
+  // Problem 2
+  function removeDuplicates(param: numberArrar): numberArrar {
+    let sortArray: numberArrar = param.slice().sort((a, b) => a - b);
+    let getSingle: numberArrar = [];
+
+    for (let index = 0; index < sortArray.length; index++) {
+      if (!(sortArray[index] === sortArray[index - 1])) {
+        getSingle.push(sortArray[index]);
+      }
+    }
+    return getSingle;
+  }
+  console.log(removeDuplicates([1, 6, 2, 2, 3, 3, 4, 4, 4, 5, 4, 4, 1, 1]));
 
   //
 }
