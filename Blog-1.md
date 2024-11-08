606596257e08ada1979de7d47653978852e4d9bc
MD Sadid <sfhsadid@gmail.com>
2024-11-08 09:37:31 +0300

    Blog 1 done

---
+++ Blog-1.md
@@ -0,0 +1,3 @@
+## The significance of union and intersection types in Typescript.
+
+Union and Intersection are powerful items in TypeScript. Union helps me choose which one I get. Intersection gives me 2, 3, 4, etc., which I have to use on this. Among these features, union and intersection types are particularly useful for modeling complex data structures and expressing flexible type relationships. I really enjoy using them on my project. They've helped me a lot and solved more big problems.
--- src/index.ts
+++ src/index.ts
@@ -126,5 +126,27 @@
   const car = new Car("Honda", "Civic", 2018);
   // car.getCarAge();
 
+  // Problem 8
+  interface Profile2 {
+    name: string;
+    age: number;
+    email: string;
+  }
+
+  const person2: Profile2 = {
+    name: "Alice",
+    age: 25,
+    email: "alice@example.com",
+  };
+
+  function validateKeys<T extends object>(
+    myObj: T,
+    keyArray: (keyof T)[]
+  ): boolean {
+    return keyArray.every((key) => key in myObj);
+  }
+
+  console.log(validateKeys(person2, ["name", "age"]));
+
   //
 }
